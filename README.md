0d0a6ca38006d923c63af2ada2d2950f27f2ae8e
koen-github <koen@koen-mint.(none)>
2013-09-16 17:41:51 +0200

    Update: Readme

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ GnotimeScala
 Pattern matching and XML processing from GnomeTime to Scala model
 
 import into netbeans or something like that.
+
+Main class: printDataModel.scala 
--- model/timetrackingmodel.scala
+++ model/timetrackingmodel.scala
@@ -7,6 +7,7 @@ object Types
 import Types._
 
 case class Project(  title:String, // in gnotime.xml: <title>
+                     projectDesc:String,
                      id:String, // in gnotime.xml: <id>
                      tasks:List[TaskList] // in gnotime.xml: <project-list>
                     
@@ -14,9 +15,8 @@ case class Project(  title:String, // in gnotime.xml: <title>
 {
 }
 
-  case class TaskList(  guid:String, // in gnotime.xml: <title>
-                     memo:String, // in gnotime.xml: <id>
-                     
+  case class TaskList(  memo:String, // in gnotime.xml: <id>
+                     guid:String, // in gnotime.xml: <title>
                      timeIntervals2:List[TimeInterval] // in gnotime.xml: <task-list> (not certain yet, better verify) list of time intervals on which this project was active tracked on TOP level (so excluding time intervals occupied by the subprojects).
                   )
 {
